a7fcc9244cfa92a11d669a75aafb80f87f4cb388
Collin Westenhaver <collin.westenhaver@icloud.com>
2019-02-16 20:42:57 -0800

    Initial Commit to create solution

---
+++ IssueTracker/IssueTrackerWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace IssueTracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class IssueTrackerWindow : Window
+    {
+        public IssueTrackerWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
